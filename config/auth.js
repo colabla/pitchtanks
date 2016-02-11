7413ae94d70c6f5a01ca8933795973cdefebe941
George Witwer <george@colab.la>
2016-02-11 14:06:29 -0500

    ??

--- config/auth.js
+++ config/auth.js
@@ -1,20 +1,21 @@
+const base = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'http://pitchtanks-colab.herokuapp.com';
 
 module.exports = {
   facebookAuth: {
     clientID: '536185809877448',
     clientSecret: '66b8a62da47b1742e898b560b135ec74',
-    callbackURL: 'http://localhost:3000/auth/facebook/callback',
+    callbackURL: base + '/auth/facebook/callback', // eslint-disable-line prefer-template
   },
 
   twitterAuth: {
     consumerKey: 'kreAVjA9fRp4dPdGlteeGjExR',
     consumerSecret: 'zafeyozClihvzI5RLKLCE3FaNzIGqM8cyYUlOMrmbSvMVBn6Ng',
-    callbackURL: 'http://localhost:3000/auth/twitter/callback',
+    callbackURL: base + '/auth/twitter/callback', // eslint-disable-line prefer-template
   },
 
   linkedInAuth: {
     consumerKey: '78n3vx3os3v55x',
     consumerSecret: 'WeizMUL29MDPKcDN',
-    callbackURL: 'http://localhost:3000/auth/linkedin/callback',
+    callbackURL: base + '/auth/linkedin/callback', // eslint-disable-line prefer-template
   },
 };
