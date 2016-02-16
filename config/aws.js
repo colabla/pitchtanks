9612f19c1cc0b1d792f80982dbf5925349cd80ef
George Witwer <george@colab.la>
2016-02-16 17:27:03 -0500

    updated gitignore

---
+++ config/aws.js
@@ -0,0 +1,11 @@
+module.exports = (env) => {
+  if(env === 'development'){
+      const secrets = require('./secrets.js');
+      return secrets;
+  }
+  return {
+    aws_access_key_id: process.env.AWS_ACCESS_KEY,
+    aws_secret_access_key: process.env.AWS_SECRET_KEY,
+    s3_bucket: process.env.S3_BUCKET,
+  };
+};
