d66fcbb790acc14151739e01a9b7f467c1e277c8
George Witwer <george@colab.la>
2016-02-16 00:09:40 -0500

    picture upload work

--- app/client/controllers/campaign.js
+++ app/client/controllers/campaign.js
@@ -1,42 +1,39 @@
 const campaignController = () => {
   return ['$scope', '$state', 'Upload', '$timeout',
-    function (                             // eslint-disable-line func-names
-      $scope, $state, Upload, $timeout
-    ) {
-      $scope.timer = undefined;
-      $scope.triggerUploadOn = (e, id) => {
-        e.stopPropagation();
-        $scope.timer = $timeout(() => {
-          console.log('click');
-          $(`#${id}`).trigger('click');
-        }, 100);
-      };
+  function (                             // eslint-disable-line func-names
+    $scope, $state, Upload, $timeout
+  ) {
+    $scope.timer = undefined;
+    
+    $scope.uploadFiles = (file, errFiles) => {
+      console.log('here');
+      $scope.f = file;
+      $scope.errFile = errFiles && errFiles[0];
+      if (file) {
+        console.log(file);
+        file.upload = Upload.upload({
+          url: '/api/uploadPhoto',
+          data: { file },
+        });
 
-      $scope.$on('$destroy', () => { if ($scope.timer) { $timeout.cancel($scope.timer); } });
-
-      $scope.uploadFiles = (file, errFiles) => {
-        console.log('here');
-        $scope.f = file;
-        $scope.errFile = errFiles && errFiles[0];
-        if (file) {
-            file.upload = Upload.upload({
-                url: '/api/uploadPhoto',
-                data: { file },
-            });
-
-            file.upload.then((response) => {
-                $timeout(() => {
-                    file.result = response.data;
-                });
-            }, (response) => {
-              if (response.status > 0) {
-                $scope.errorMsg = `${response.status}: ${response.data}`;
-              }
-            }, (evt) => {
-                file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total, 10));
-            });
-        }
-      };
-    },
-  ];
+        file.upload.then((response) => {
+          console.log(`RES: ${response}`);
+          $timeout(() => {
+            console.log(`RES: ${response}`);
+            file.result = response.data;
+          });
+        }, (response) => {
+          console.log(`RES: ${response}`);
+          if (response.status > 0) {
+            $scope.errorMsg = `${response.status}: ${response.data}`;
+            console.log(`ERROR: ${$scope.errorMsg}`);
+          }
+        }, (evt) => {
+          file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total, 10));
+          console.log(`PROGRESS: ${file.progress}`);
+        });
+      }
+    };
+  },
+];
 };
