4382797a073aa5b8e9f18e075a4607d8b83c705d
George Witwer <george@colab.la>
2016-02-25 02:17:45 -0500

    undo

--- app/server/api.js
+++ app/server/api.js
@@ -174,18 +174,14 @@ module.exports = (db) => {
           return done(err);
         }
 
-        if (!campaign.upvotes.includes(req.params.user)) {
-          campaign.upvotes.push(req.params.user);
-        }
+        campaign.upvotes.push(req.params.user);
         campaign.upvoteCount = campaign.upvotes.length;
         campaign.save((err) => {
           if (err) {
             throw err;
           }
           db.User.findOne({ _id: req.params.user }, (err, user) => {
-            if (!user.upvotes.includes(req.params.campaign)) {
-              user.upvotes.push(req.params.campaign);
-            }
+            user.upvotes.push(req.params.campaign);
             user.save((err) => {
               if (err) {
                 throw err;
